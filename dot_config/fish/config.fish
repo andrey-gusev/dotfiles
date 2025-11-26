6e7fb354ac48ea6fb5ea685a5abb4e22ef79cb4d
Andrey Gusev <Anderson14766@mail.ru>
2025-11-26 21:43:50 +0500

    some changes

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,47 +1,45 @@
-if status is-interactive
-    fish_vi_key_bindings
-    set fish_greeting ""
-    set -gx EDITOR nvim
-    set -gx TERMINAL foot
-    set -gx BROWSER qutebrowser
-    set -gx XDG_CONFIG_HOME $HOME/.config
-    set -gx XDG_DATA_HOME $HOME/.local/share
-    set -gx XDG_CACHE_HOME $HOME/.cache
-    set -gx XAUTHORITY /tmp/Xauthority
-    set -gx PASSWORD_STORE_DIR "$XDG_DATA_HOME/password-store"
-    set -gx PATH $PATH $HOME/.local/bin
-    set -gx PATH $PATH $HOME/.config/emacs/bin
+fish_vi_key_bindings
+set fish_greeting ""
+set -gx EDITOR nvim
+set -gx TERMINAL foot
+set -gx BROWSER qutebrowser
+set -gx XDG_CONFIG_HOME $HOME/.config
+set -gx XDG_DATA_HOME $HOME/.local/share
+set -gx XDG_CACHE_HOME $HOME/.cache
+set -gx XAUTHORITY /tmp/Xauthority
+set -gx PASSWORD_STORE_DIR "$XDG_DATA_HOME/password-store"
+set -gx PATH $PATH $HOME/.local/bin
+set -gx PATH $PATH $HOME/.config/emacs/bin
 
-    if test -z $DISPLAY; and test $XDG_VTNR -eq 1
-        exec Hyprland
-    end
-
-    abbr -a --position anywhere -- --help '--help | bat -plhelp'
-    abbr -a --position anywhere -- -h '-h | bat -plhelp'
-    abbr p "sudo pacman"
-    abbr man "batman"
-    abbr cat "bat"
-    abbr v "nvim"
-    abbr sdn "shutdown now"
-    abbr sys "sudo systemctl"
-    abbr g "git"
-    abbr gs "git status"
-    abbr ga "git add"
-    abbr gc "git commit -m"
-    abbr gp "git push"
-    abbr gr "git remote set-url origin git@github.com:andrey-gusev/"
-    abbr t "sudo tailscale"
-    abbr e "emacs"
-    abbr y "yay"
-    abbr cdc "cd ~/.local/share/chezmoi/"
-    abbr cf "chezmoi edit ~/.config/"
-    abbr sc "chezmoi edit ~/.local/bin/"
-    abbr ca "chezmoi apply" 
-    abbr cfh "chezmoi edit ~/.config/hypr/hyprland.conf"
-    abbr cfv "chezmoi edit ~/.config/nvim/init.vim"
-    abbr cfk "chezmoi edit ~/.config/kitty/kitty.conf"
-    abbr cff "chezmoi edit ~/.config/fish/config.fish"
-    abbr cfw "chezmoi edit ~/.config/waybar/config"
-    abbr cfws "chezmoi edit ~/.config/waybar/style.css"
+if test -z $DISPLAY; and test $XDG_VTNR -eq 1
+    exec Hyprland
 end
-starship init fish | source
+
+# abbr -a --position anywhere -- --help '--help | bat -plhelp'
+# abbr -a --position anywhere -- -h '-h | bat -plhelp'
+abbr p "sudo pacman"
+abbr man "batman"
+# abbr cat "bat"
+abbr v "nvim"
+abbr sdn "shutdown now"
+abbr sys "sudo systemctl"
+abbr g "git"
+abbr gs "git status"
+abbr ga "git add"
+abbr gc "git commit -m"
+abbr gp "git push"
+abbr gd "git diff"
+abbr gr "git remote set-url origin git@github.com:andrey-gusev/"
+abbr t "sudo tailscale"
+abbr e "emacs"
+abbr y "yay"
+abbr cdc "cd ~/.local/share/chezmoi/"
+abbr cf "chezmoi edit ~/.config/"
+abbr sc "chezmoi edit ~/.local/bin/"
+abbr ca "chezmoi apply" 
+abbr cfh "chezmoi edit ~/.config/hypr/hyprland.conf"
+abbr cfv "chezmoi edit ~/.config/nvim/init.vim"
+abbr cfk "chezmoi edit ~/.config/kitty/kitty.conf"
+abbr cff "chezmoi edit ~/.config/fish/config.fish"
+abbr cfw "chezmoi edit ~/.config/waybar/config"
+abbr cfws "chezmoi edit ~/.config/waybar/style.css"
