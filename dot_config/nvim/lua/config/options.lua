d6d1a1732095d7e9ca6da41179d8f4d1ba954e71
Andrey Gusev <Anderson14766@mail.ru>
2026-06-22 17:54:25 +0500

    удалён конфиг для nvim. для него создан отдельный репозиторий

--- dot_config/nvim/lua/config/options.lua
+++
@@ -1,4 +0,0 @@
-vim.opt.wrap = true -- Включаем визуальный перенос длинных строк
-vim.opt.linebreak = true -- Переносим слова целиком, а не режем их посреди буквы
-vim.opt.shiftwidth = 4
-vim.opt.spelllang = { "en", "ru" }
