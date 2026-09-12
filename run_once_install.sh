62df5514efef576d1daadc27e66709e661cb743e
Andrey Gusev <Anderson14766@mail.ru>
2026-09-12 21:39:32 +0500

    добавлена проверка наличия директории с цветами pywal, чтобы hyprland при первом запуске не падал с ошибками

--- run_once_install.sh
+++ run_once_install.sh
@@ -184,6 +184,12 @@ change_shell() {
   fi
 }
 
+check_wal_cache() {
+  if [ ! -d "$HOME/.cache/wal" ]; then
+    setbg
+  fi
+}
+
 main() {
   check_list_file
   check_arch_system
@@ -192,6 +198,7 @@ main() {
   install_packages
   enable_daemons
   change_shell
+  check_wal_cache
 }
 
 main "$@"
